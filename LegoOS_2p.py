f3a1053e896b04a770b23b04430ed60ba76281ab
Yiliang Wan <yiliangw@hotmail.com>
2023-08-16 16:35:30 +0000

    Update SMP configuration for 2p exp

--- LegoOS_2p.py
+++ LegoOS_2p.py
@@ -3,73 +3,12 @@ Simple LegoOS experiment, which sets up a pComponent and a mComponent connected
 through ethernet.
 '''
 from simbricks.orchestration.experiments import Experiment
-from simbricks.orchestration.nodeconfig import IdleHost, LinuxNode
-from simbricks.orchestration.simulators import QemuHost, E1000NIC, SwitchNet
+from simbricks.orchestration.simulators import E1000NIC, SwitchNet
 
+import sys
 import os
-import math
-
-
-class LegoOSQemuHost(QemuHost):
-
-    def __init__(self, kernel_path, debug=False):
-        config = LinuxNode()
-        config.app = IdleHost()
-        super().__init__(config)
-        # self.sync_period = 500000
-        self.kernel_path = kernel_path
-        self.debug = debug
-
-    def run_cmd(self, env):
-        accel = ',accel=kvm:tcg' if not self.sync else ''
-        if self.node_config.kcmd_append:
-            kcmd_append = ' ' + self.node_config.kcmd_append
-        else:
-            kcmd_append = ''
-
-        '''
-        The command from LegoOS
-        '''
-        cmd = (
-            f'{env.qemu_path} -serial mon:stdio '
-            '-cpu Skylake-Server -display none -nic none -no-reboot '
-            f'-kernel {self.kernel_path} '
-            '-append "earlyprintk=ttyS0 console=ttyS0 memmap=2G$4G" '
-            f'-m 8G -smp 4 '
-        )
-
-        if self.debug:
-            cmd += ' -s -S '
-
-        if self.sync:
-            unit = self.cpu_freq[-3:]
-            if unit.lower() == 'ghz':
-                base = 0
-            elif unit.lower() == 'mhz':
-                base = 3
-            else:
-                raise ValueError('cpu frequency specified in unsupported unit')
-            num = float(self.cpu_freq[:-3])
-            shift = base - int(math.ceil(math.log(num, 2)))
-
-            cmd += f' -icount shift={shift},sleep=off '
-
-        for dev in self.pcidevs:
-            cmd += f'-device simbricks-pci,socket={env.dev_pci_path(dev)}'
-            if self.sync:
-                cmd += ',sync=on'
-                cmd += f',pci-latency={self.pci_latency}'
-                cmd += f',sync-period={self.sync_period}'
-            else:
-                cmd += ',sync=off'
-            cmd += ' '
-
-        # qemu does not currently support net direct ports
-        assert len(self.net_directs) == 0
-        # qemu does not currently support mem device ports
-        assert len(self.memdevs) == 0
-        return cmd
-
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from components import LegoOSQemuHost
 
 SYNC = False
 SYNC_MODE = 2 if SYNC else 0
@@ -82,7 +21,7 @@ node_0_img_path = img_prefix + '/2p_node_0.bzImage'
 node_1_img_path = img_prefix + '/2p_node_1.bzImage'
 
 # node_0
-node_0 = LegoOSQemuHost(node_0_img_path, debug=False)
+node_0 = LegoOSQemuHost(node_0_img_path, memory='8G', cores=8, debug=False)
 node_0.sync = SYNC
 node_0.name = 'node_0'
 node_0.wait = True
@@ -96,7 +35,7 @@ node_0.add_nic(node_0_nic)
 e.add_nic(node_0_nic)
 
 # node_1
-node_1 = LegoOSQemuHost(node_1_img_path, debug=False)
+node_1 = LegoOSQemuHost(node_1_img_path, memory='8G', cores=8, debug=False)
 node_1.sync = SYNC
 node_1.name = 'node_1'
 node_1.wait = True
